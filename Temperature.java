caa6c212dbc6f5bfedd65e48d93471c0c31cf6d3
Michael <michael@testmunk.com>
2016-11-19 20:00:47 -0800

    update file
    add data

--- Temperature.java
+++ Temperature.java
@@ -1,7 +1,11 @@
 // Michael Walsh 2016 
-
+import javax.swing.*;
 public class Temperature {
-	private int currentTemp, minTemp, maxTemp;
-	JButton incTemp, decTemp;
-	JLabel currentTemp;
+	/* data */
+	private int currTemp, minTemp, maxTemp; //temperature data
+	private JButton incCurr, decCurr, incMin, decMin, incMax, decMax; //buttons to edit temps
+	private JLabel currT, minT, maxT; //display labels
+	private JLabel temperatureLabel; //to label the temperature section
+	/* methods */
+	
 }
