31c55eb44c98fd9e0a433118325e8aeb6e7d1d20
ryanf3 <r2fernandez@scu.edu>
2016-11-21 14:35:42 -0800

    Add files via upload

--- Sprinkler.java
+++ Sprinkler.java
@@ -4,4 +4,19 @@ public class Sprinkler {
 	private boolean active, working; //status of sprinkler
 	
 	/* methods */
+	public void setActive (boolean setting){
+		active = setting;
+	}
+	
+	public void setWorking (boolean settingW){
+		working = settingW;
+	}
+	
+	public boolean getActive (){
+		return active;
+	}
+	
+	public boolean getWorking (){
+		return working;
+	}
 }
