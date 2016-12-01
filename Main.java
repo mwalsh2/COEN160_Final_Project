6c03db5f957b1bf8b3dc594753e9b9585328f38d
ryanf3 <r2fernandez@scu.edu>
2016-11-30 17:15:45 -0800

    Major Changes
    rfgh

--- Main.java
+++ Main.java
@@ -1,25 +1,24 @@
 import java.util.*;
+import javax.swing.*;
+
+import javax.swing.SwingUtilities;
 public class Main {
 	public static void main(String[] args) {
 		
-		/* put display in here */
 		
+		/* put display in here */
+
 		Garden realGarden = new Garden();
 		Schedule realSchedule = new Schedule();
+		Water realWater = new Water();
+		Temperature realTemperature = new Temperature();
 		
-		//pass to garden setter the amount of time scheduled to run
-		realGarden.setTodayG(realSchedule.getTimeRun());
-		
-		/* update water ever 24 hours */
-		Timer timer = new Timer ();
-		TimerTask dailyTask = new TimerTask () {
-		    @Override
-		    public void run () {
-		        realGarden.getWater().slideWaterUse();
-		    }
-		};
-		// schedule the task to run starting now and then every day
-		timer.schedule(dailyTask, 1000*60*60*24, 1000*60*60*24);
-		/* *** */
+		/*Initiate program*/
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Display();
+			}
+		});
 	}
 }
--- Temperature.java
+++ Temperature.java
@@ -3,9 +3,7 @@ import javax.swing.*;
 public class Temperature {
 	/* data */
 	private int currTemp, minTemp, maxTemp; //temperature data
-	private JButton incCurr, decCurr, incMin, decMin, incMax, decMax; //buttons to edit temps
-	private JLabel currT, minT, maxT; //display labels
-	private JLabel temperatureLabel; //to label the temperature section
+	
 	
 	/* constructor */
 	Temperature(){
